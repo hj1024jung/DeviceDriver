8a9a9207153edf9a6771966c45726e3c134e8a17
hj1024.jung <flksignal@gmail.com>
2023-07-07 13:40:33 +0900

    read enable

--- Project1/DeviceDriver.cpp
+++ Project1/DeviceDriver.cpp
@@ -1,12 +1,38 @@
+#include <stdexcept>
+#include <Windows.h>
 #include "DeviceDriver.h"
+
+using namespace std;
+
+class ReadFailException : public exception
+{
+public:
+    explicit ReadFailException()
+		: exception("__READ_EXCEPTION_OCCURED__")
+    {}
+};
+
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    const int firstReadVal = m_hardware->read(address);
+
+    for (int loop = 0; loop < (ReadTryCount - 1); loop++)
+    {
+        Sleep(ReadIssueIntervalMilliSec);
+
+        int readVal = m_hardware->read(address);
+
+    	if (firstReadVal != readVal)
+	    {
+            throw ReadFailException();
+	    }
+    }
+
+    return firstReadVal;
 }
 
 void DeviceDriver::write(long address, int data)
--- Project1/DeviceDriver.h
+++ Project1/DeviceDriver.h
@@ -4,10 +4,15 @@
 class DeviceDriver
 {
 public:
+    static const int ReadTryCount = 5;
+
     DeviceDriver(FlashMemoryDevice* hardware);
     int read(long address);
     void write(long address, int data);
 
 protected:
+    static const int ReadIssueIntervalMilliSec = 200;
+
+
     FlashMemoryDevice* m_hardware;
 };
